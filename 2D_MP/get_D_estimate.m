bc68bd49004b62de2a442312557e1f11199a2925
Alan Boles <33553493+alanewing@users.noreply.github.com>
2025-09-10 16:18:33 -0700

    Add files via upload

--- 2D_MP/get_D_estimate.m
+++ 2D_MP/get_D_estimate.m
@@ -1,4 +1,4 @@
-function [D, variance, chain] = get_D_estimate(M, t_cell, t_global, wx_cell, wy_cell, ...
+function [D_mean, D_std, D_median, D_iqr, chain] = get_D_estimate(M, t_cell, t_global, wx_cell, wy_cell, ...
                                                hxx_cell, hyy_cell, hxy_cell,...
                                                units_time, units_dist, flag_anchor, ...
                                                chain_length)
@@ -30,11 +30,12 @@ function [D, variance, chain] = get_D_estimate(M, t_cell, t_global, wx_cell, wy_
     chain = chainer_main(chain, chain_length, [], true, true, flag_anchor);
     
     %% remove burn
-    idx = 500 : chain.length ;
+    idx = ceil(chain.length/2) : chain.length ;
     
-    D =        mean(chain.D(idx));
-    variance = var(chain.D(idx));
-
+    D_mean   = mean(chain.D(idx));
+    D_std    = std(chain.D(idx));
+    D_median = median(chain.D(idx));
+    D_iqr    = iqr(chain.D(idx));
     % make noise when done
     beep
 end
